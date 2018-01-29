914b9903231ddf4c99d604bcaeeaaf66e008aafd
Lukasz Szmyga <lukasz.szmyga@gmail.com>
2018-01-29 16:34:38 +0100

    ajax calls

--- main.js
+++ main.js
@@ -1,19 +1,21 @@
 ﻿var pageCounter = 1;
 var peopleInfo = document.getElementById("personal-info");
 var btn = document.getElementById("btn");
-
-btn.addEventListener("click", function() {
-  var ourRequest = new XMLHttpRequest();
-  ourRequest.open("GET", "https://raw.githubusercontent.com/LukaszSzmyga/json-example/master/people-" + pageCounter + ".json");
-  ourRequest.onload = function () {
-      var ourData = JSON.parse(ourRequest.responseText);
-      appendUsers(ourData);
-  };
-  ourRequest.send();
-  pageCounter++;
-  if (pageCounter > 4) {
-    btn.classList.add("hide-me");
-  }
+      
+document.addEventListener("DOMContentLoaded", function() {
+  btn.addEventListener("click", function() {;
+    var ourRequest = new XMLHttpRequest();
+    ourRequest.open("GET", "https://raw.githubusercontent.com/LukaszSzmyga/json-example/master/people-" + pageCounter + ".json");
+    ourRequest.onload = function () {
+        var ourData = JSON.parse(ourRequest.responseText);
+        appendUsers(ourData);
+    };
+    ourRequest.send();
+    pageCounter++;
+    if (pageCounter > 4) {
+      btn.classList.add("hide-me");
+    };
+  });
 });
 
 function appendUsers(data) {
@@ -25,5 +27,3 @@ function appendUsers(data) {
 
   peopleInfo.insertAdjacentHTML("beforeend", htmlString);
 };
-
-document.addEventListener("DOMContentLoaded", main);
