b44598aa91bebf7adb3f69ac15281b330c2d7dfb
Lukasz Szmyga <lukasz.szmyga@gmail.com>
2018-01-29 18:07:54 +0100

    ajax calls

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-﻿var pageCounter = 1;
-var peopleInfo = document.getElementById("personal-info");
-var btn = document.getElementById("btn");
-      
-document.addEventListener("DOMContentLoaded", function() {
-  btn.addEventListener("click", function() {;
+﻿function main() {
+  var pageCounter = 1;
+  var peopleInfo = document.getElementById("personal-info");
+  var btn = document.getElementById("btn");
+     
+  btn.addEventListener("click", function() {
     var ourRequest = new XMLHttpRequest();
     ourRequest.open("GET", "https://raw.githubusercontent.com/LukaszSzmyga/json-example/master/people-" + pageCounter + ".json");
     ourRequest.onload = function () {
@@ -16,10 +16,11 @@ document.addEventListener("DOMContentLoaded", function() {
       btn.classList.add("hide-me");
     };
   });
-});
-
+};
+ 
 function appendUsers(data) {
   var htmlString = "";
+  var peopleInfo = document.getElementById("personal-info");
 
   for (i = 0; i < data.length; i++) {
     htmlString += "<p>" + "My name is " + data[i].fname + " " + data[i].lname + ". I'm from " + data[i].city + " and my phone number is " + data[i].tel + ".</p>";
@@ -27,3 +28,5 @@ function appendUsers(data) {
 
   peopleInfo.insertAdjacentHTML("beforeend", htmlString);
 };
+
+document.addEventListener("DOMContentLoaded", main);
